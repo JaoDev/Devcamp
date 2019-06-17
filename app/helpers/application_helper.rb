efa0c39f7ae50d1005665dd92e41a9e2edbff0c4
Humsbel Jao <humsbel_obregon12@hotmail.com>
2019-06-17 13:26:30 -0700

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -57,4 +57,16 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator smg
+    js add_gritter(smg, title: "Jesus Almirco Porfolios", sticky: false)
+  end
+
 end
