123fc3923de545313571e9480480e03c006d7f8f
Humsbel Jao <humsbel_obregon12@hotmail.com>
2019-06-17 10:27:05 -0700

    Integrate cocoon for portfolio items nested form component and updated show page and edit page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,8 @@
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require jquery-ui
-//= require activestorage
 //= require html.sortable
+//= require cocoon
+//= require activestorage
 //= require turbolinks
 //= require_tree .
